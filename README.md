0cdd384099a356dd3c97591f61ea37f6fb5dfba4
Taigo <taigo@eccodx.com>
2021-05-23 20:11:35 -0300

    meu primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# App calculadora bacana!
+
+Aplicacao para estudo de *GIT* com a [EccoDX](http://eccodx.com)
---
+++ app.py
@@ -0,0 +1,6 @@
+#Calculadora
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("Operações basicas possíveis:\n - Soma: {}\n - Subtração: {}\n\nValeu!".format(numero1 + numero2,numero1 - numero2))
