ad9641dee12c38af17f4183356764e8c9782ae5a
Taigo <taigo@eccodx.com>
2021-05-23 20:19:38 -0300

    feat multiplicacao

--- app.py
+++ app.py
@@ -3,4 +3,4 @@
 numero1 = int(input("Digite o primeiro número: "))
 numero2 = int(input("Digite o segundo número: "))
 
-print("Operações basicas possíveis:\n - Soma: {}\n - Subtração: {}\n\nValeu!".format(numero1 + numero2,numero1 - numero2))
+print("Operações basicas possíveis:\n - Soma: {}\n - Subtração: {}\n - Multiplicacao: {}\n\nValeu!".format(numero1 + numero2,numero1 - numero2,numero1 * numero2))
